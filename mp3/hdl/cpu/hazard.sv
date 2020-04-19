c2ca5bb28d2ba964ed6704f4337c3401dfbaa5f0
Damian Mirecki <mirecki2@illinois.edu>
2020-04-19 16:55:51 -0500

    Hazard and Fwd

---
+++ mp3/hdl/cpu/hazard.sv
@@ -0,0 +1,18 @@
+import rv32i_types::*;
+
+module hazard_detection
+(
+    input logic mem_read,
+    input rv32i_word rs1_IFID,
+    input rv32i_word rs2_IFID,
+    input rv32i_word rs2_IDEX,
+    output logic hazard_stall
+);
+
+always_comb
+begin
+    if(mem_read && ((rs2_IDEX == rs2_IFID) || (rs2_IFID == rs2_IDEX))) stall = 1;
+    else stall = 0;
+end
+
+endmodule : hazard_detection
